d44d55c959ba656b96b03f6eb90f631aed59abe8
imndx <imndx@gmail.com>
2019-11-19 18:25:13 +0800

    fix remove conversation issue

--- src/js/ui/stores/sessions.js
+++ src/js/ui/stores/sessions.js
@@ -84,13 +84,12 @@ class sessions {
 
     @action removeConversation(conversation) {
 
-        let conversations = self.conversations.filter(e => !e.conversation.equal(conversation));
-        self.conversations.replace(conversations);
+        self.conversations = self.conversations.filter(e => !e.conversation.equal(conversation.conversation));
 
         wfc.removeConversation(conversation, true);
 
         updateMenus({
-            conversations: conversations.slice(0, 10)
+            conversations: self.conversations.slice(0, 10)
         });
     }
 
