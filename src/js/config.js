b7a77b63ef8666496452fb049e53ee1925f42eef
imndx <imndx@gmail.com>
2019-11-21 18:55:34 +0800

    update proto

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,8 @@ export default class Config {
     static QR_CODE_PREFIX_PC_SESSION = "wildfirechat://pcsession/";
 
     static WEB_APP_ID = 'web_12345678';
-    static WEB_APP_KEY = 'daa0390ce338f3b5075b0ab38fd081aae802b4bf';
+    static WEB_APP_KEY = '44889f61b0c4908761953fd178451b4e80308368';
+    static MESSAGE_ROAMING = 1;
 
     static SDK_PLATFORM_PC = 0;
     static SDK_PLATFORM_WEB = 1;
