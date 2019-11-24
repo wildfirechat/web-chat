8d2f74c925818c0b89c0b07cb0ea52a92f468999
imndx <imndx@gmail.com>
2019-11-24 20:12:14 +0800

    support pull roaming message config.

--- src/js/config.js
+++ src/js/config.js
@@ -4,7 +4,10 @@ export default class Config {
 
     static WEB_APP_ID = 'web_12345678';
     static WEB_APP_KEY = '44889f61b0c4908761953fd178451b4e80308368';
+
     static MESSAGE_ROAMING = 1;
+    // 拉取最近30天的消息
+    static MESSAGE_ROAMING_DAY_COUNT = 30;
 
     static SDK_PLATFORM_PC = 0;
     static SDK_PLATFORM_WEB = 1;
