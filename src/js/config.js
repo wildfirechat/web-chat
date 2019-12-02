aa285fe05f4c32b713b1427ef2597efe94c2c89c
imndx <imndx@gmail.com>
2019-12-02 23:27:00 +0800

    update readme and etc

--- src/js/config.js
+++ src/js/config.js
@@ -1,14 +1,16 @@
 import { isElectron } from './platform'
 export default class Config {
-    static APP_SERVER = 'http://web.wildfirechat.cn';
+    static APP_SERVER = 'http://wildfirechat.cn';
+    // 用于本地打包验证
+    //static APP_SERVER = 'http://localhost:8888';
     static QR_CODE_PREFIX_PC_SESSION = "wildfirechat://pcsession/";
 
     static WEB_APP_ID = 'web_12345678';
     static WEB_APP_KEY = '44889f61b0c4908761953fd178451b4e80308368';
 
     static MESSAGE_ROAMING = 1;
-    // 拉取最近30天的消息
-    static MESSAGE_ROAMING_DAY_COUNT = 30;
+    // 拉取最近2小时的消息
+    static MESSAGE_ROAMING_HOUR_COUNT = 2;
 
     static getWFCPlatform() {
         if (isElectron()) {
