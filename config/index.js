fd289a7a8e794dd6a2bb6d97a36aae5fa0b931f8
imndx <imndxx@gmail.com>
2020-03-15 21:54:49 +0800

    update config and proto

--- config/index.js
+++ config/index.js
@@ -10,8 +10,9 @@ const config = {
     assets: path.resolve(__dirname, '../src/assets'),
     dist: path.resolve(__dirname, '../dist'),
 
-    // fixme 部署到服务器时，下面需要改成你们自己的服务器地址，**注意端口号**
-    STATIC_RESOURCE_PATH: 'https://static.wildfirechat.cn:80/',
+    // fixme 部署到服务器时，下面需要改成你们自己的服务器地址，如果需要使用非80端，，一定要带上端口信息, 如:
+    //STATIC_RESOURCE_PATH: 'https://static.wildfirechat.cn:8080/',
+    STATIC_RESOURCE_PATH: 'https://static.wildfirechat.cn/',
 };
 
 export default config;
