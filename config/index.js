046376f62dc03a36762ea42364b3ee4d55f92562
x86 <1960867973@qq.com>
2020-03-20 17:57:14 +0800

    update readme

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ const config = {
     assets: path.resolve(__dirname, '../src/assets'),
     dist: path.resolve(__dirname, '../dist'),
 
-    // fixme 部署到服务器时，下面需要改成你们自己的服务器地址，如果需要使用非80端，，一定要带上端口信息, 如:
+    // fixme 部署到服务器时，下面需要改成你们自己的服务器地址，如果需要使用非80端，一定要带上端口信息, 如:
     //STATIC_RESOURCE_PATH: 'https://static.wildfirechat.cn:8080/',
     STATIC_RESOURCE_PATH: 'https://static.wildfirechat.cn/',
 };
