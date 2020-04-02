edbc335021755ec843ba5a6347a2261315a99c0e
x86 <1960867973@qq.com>
2020-04-02 14:31:05 +0800

    update comment

--- src/js/config.js
+++ src/js/config.js
@@ -8,12 +8,13 @@ export default class Config {
 
     static DEFAULT_PORTRAIT_URL = 'https://static.wildfirechat.cn/user-fallback.png';
     // 如果需要支持音视频通话功能，必须全站使用https(包括app server和im server) + wss，
-    // WebSockets over SSL/TLS，启用https时，一定为true；不启用https，为false
+    // WebSockets over SSL/TLS，启用https时，一定要配置为true；不启用https，可为false
     static USE_WSS = true;
     // WSS 端口
     static WSS_PORT = 8084;
 
     // APP SERVER的地址，启用https时，APP SERVER也需要支持https
+    // 默认的app server使用端口是8888
     static APP_SERVER = 'https://app.wildfirechat.cn';
     static QR_CODE_PREFIX_PC_SESSION = "wildfirechat://pcsession/";
     static ICE_ADDRESS = 'turn:turn.wildfirechat.cn:3478';
@@ -21,6 +22,7 @@ export default class Config {
     static ICE_PASSWORD = 'wfchat';
     static LANGUAGE = 'zh_CN';
 
+    // appId和appKey和专业版im server是绑定的，一定要做对应修改
     static WEB_APP_ID = 'web_12345678';
     static WEB_APP_KEY = '44889f61b0c4908761953fd178451b4e80308368';
 
