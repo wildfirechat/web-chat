9df5cff1aa1f2804c374b5dd60df7426bc4f1708
imndx <imndxx@gmail.com>
2020-07-20 11:13:05 +0800

    update proto

--- src/js/wfc/messages/messageContentType.js
+++ src/js/wfc/messages/messageContentType.js
@@ -41,4 +41,6 @@ export default class MessageContentType {
     static VOIP_CONTENT_TYPE_ADD_PARTICIPANT = 406;
     static VOIP_CONTENT_TYPE_MUTE_VIDEO = 407;
 
+    static MESSAGE_CONTENT_TYPE_FEED = 501;
+    static MESSAGE_CONTENT_TYPE_COMMENT = 502;
 }
