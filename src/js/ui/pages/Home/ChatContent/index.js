aaf8d4ff65ec15ee9bdc51238d7eaf954fbbf0c8
x86 <1960867973@qq.com>
2020-07-23 17:07:46 +0800

    fix download file issue. fix #14 fix #19

--- src/js/ui/pages/Home/ChatContent/index.js
+++ src/js/ui/pages/Home/ChatContent/index.js
@@ -715,20 +715,38 @@ export default class ChatContent extends Component {
                 // this.props.forceRerenderMessage(message.messageId);
                 ipcRenderer.send('file-download', {messageId : message.messageId, remotePath : file.remotePath});
             } else {
-                let varExt = file.remotePath.split('.');
-                if (varExt[varExt.length - 1] === "txt" || varExt[varExt.length -1] === "log") {
-                    window.open(file.remotePath);
-                }
-                else {
-                    let iframe;
-                    iframe = document.getElementById("hiddenDownloader");
-                    if (iframe == null) {
-                        iframe = document.createElement('iframe');
-                        iframe.id = "hiddenDownloader";
-                        iframe.style.visibility = 'hidden';
-                        document.body.appendChild(iframe);
-                    }
-                    iframe.src = file.remotePath;
+                // let varExt = file.remotePath.split('.');
+                // if (varExt[varExt.length - 1] === "txt" || varExt[varExt.length -1] === "log") {
+                //     window.open(file.remotePath);
+                // }
+                // else {
+                //     let iframe;
+                //     iframe = document.getElementById("hiddenDownloader");
+                //     if (iframe == null) {
+                //         iframe = document.createElement('iframe');
+                //         iframe.id = "hiddenDownloader";
+                //         iframe.style.visibility = 'hidden';
+                //         document.body.appendChild(iframe);
+                //     }
+                //     iframe.src = file.remotePath;
+                // }
+                let fileHref = file.remotePath;
+                let filename = file.name;
+                if (window.navigator.msSaveBlob) {// ie
+                    let xhr = new XMLHttpRequest();
+                    xhr.onloadstart = function () {
+                        xhr.responseType = 'blob';
+                    };
+                    xhr.onload = function () {
+                        navigator.msSaveOrOpenBlob(xhr.response, filename);
+                    };
+                    xhr.open("GET", fileHref, true);
+                    xhr.send();
+                }else {
+                    let anchor = document.createElement('a');
+                    anchor.setAttribute("download", true);
+                    anchor.setAttribute("href", fileHref);
+                    anchor.click();
                 }
             }
         }
