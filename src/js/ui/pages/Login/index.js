a6b4771867f8685fde7938667ea908b598c06b9e
x86 <1960867973@qq.com>
2020-09-06 21:29:18 +0800

    fix quit login issue

--- src/js/ui/pages/Login/index.js
+++ src/js/ui/pages/Login/index.js
@@ -55,9 +55,10 @@ export default class Login extends Component {
         let response = await axios.post('/pc_session', {
             flag: 1,
             device_name: 'web',
+            userId: userId,
             clientId: wfc.getClientId(),
             platform: Config.getWFCPlatform()
-        } );
+        }, { withCredentials:true });
         console.log('----------- createPCLoginSession', response.data);
         if (response.data) {
             let session = Object.assign(new PCSession(), response.data.result);
@@ -88,7 +89,7 @@ export default class Login extends Component {
             console.log('-------- token is empty or invalid');
             return;
         }
-        var response = await axios.post('/session_login/' + this.appToken);
+        var response = await axios.post('/session_login/' + this.appToken, "", { withCredentials:true });
         console.log('---------- login', response.data);
         if (response.data) {
             switch (response.data.code) {
