844f7a5ad330aeac2fed54cf1649dafd31a65312
imndx <imndxx@gmail.com>
2020-10-21 16:56:31 +0800

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,16 +1,13 @@
-#!/bin/sh
-if [ $# != 2 ] ; then
-  echo "USAGE: publish htmlFile jsFile"
-  exit 1;
-fi
+cp src/js/wfc/av/internal/engine-conference.min.js src/js/wfc/av/internal/engine.min.js
+npm run release
+git checkout -- src/js/wfc/av/internal/engine.min.js
 
-jsFile=$2
+indexFileName=dist/src/index.html
+jsFile=`ls dist/src/wfc.*`
 jsFileName=$(basename $jsFile)
 
-echo $1
-echo $2
+echo $indexFileName
+echo $jsFile
 echo $jsFileName
-
 ~/apps/bin/qshell-darwin-x64-v2.4.0 fput firechat $jsFileName $jsFile
-
-scp $1 dali2:/home/ubuntu/wildfirechat_web/src
+scp $indexFileName dali2:/home/ubuntu/wildfirechat_web/src
